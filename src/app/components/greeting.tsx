942004fe9ac7f6b0e95510b474417ac2ae529804
Jared Malan <jared.malan7@gmail.com>
2023-12-17 01:06:07 -0700

    greet fully functioning using context

--- src/app/components/greeting.tsx
+++ src/app/components/greeting.tsx
@@ -1,54 +1,37 @@
 'use client'
-import Link from "next/link"
-import { faUser } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import React from "react"
-import { useEffect, useState } from "react"
-
+import Link from "next/link";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { useTransactions } from "./TransactionsContext";
 
 export function Greet() {
-    const [greeting, setGreeting] = useState('Hello')
-    const [userName, setUserName] = useState('')
+    const { user } = useTransactions();
+    const [greeting, setGreeting] = useState("");
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-
-                const response = await fetch('/user.json')
-                const userData = await response.json()
-                const { name } = userData
-
-                setUserName(name)
-            } catch (error) {
-                console.error('Error fetching data: ', error)
-            }
-        }
-        fetchData()
-    }, [])
-
-    useEffect(() => {
-        const currentHour = new Date().getHours()
+        const currentHour = new Date().getHours();
 
         if (currentHour >= 5 && currentHour < 12) {
-            setGreeting(`Good Morning, ${userName}`)
+            setGreeting(`Good Morning, ${user.name}`);
         } else if (currentHour >= 12 && currentHour < 18) {
-            setGreeting(`Good Afternoon, ${userName}`)
+            setGreeting(`Good Afternoon, ${user.name}`);
         } else if (currentHour >= 18 && currentHour < 20) {
-            setGreeting(`Good Evening, ${userName}`)
+            setGreeting(`Good Evening, ${user.name}`);
         } else {
-            setGreeting(`Hello, ${userName}`)
+            setGreeting(`Hello, ${user.name}`);
         }
-    }, [userName])
-
+    }, [user]);
 
     return (
         <div className="flex justify-between place-items-center">
             <span className="font-bold">{greeting}</span>
             <Link href="/user">
-                <FontAwesomeIcon icon={faUser} className="text-white bg-meteorite p-2 rounded-full cursor-pointer mr-6" />
-
+                <FontAwesomeIcon
+                    icon={faUser}
+                    className="text-white bg-meteorite p-2 rounded-full cursor-pointer mr-6"
+                />
             </Link>
         </div>
-    )
+    );
 }
-
